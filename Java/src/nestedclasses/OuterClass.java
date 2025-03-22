b23bff8262a96a4129cf72eb6c5f9021ed7c7914
Asad <asadmohammad8259@gmail.com>
2025-03-22 19:01:41 +0530

    file added

---
+++ Java/src/nestedclasses/OuterClass.java
@@ -0,0 +1,34 @@
+package nestedclasses;
+
+public class OuterClass
+{
+	void show()
+	{
+		System.out.println("OuterClass show");
+	}
+	
+	void demo()
+	{
+		System.out.println("OuterClass demo");
+	}
+	
+	class InnerClass
+	{
+		void display()
+		{
+			System.out.println("InnerClass display");
+		}
+		
+		void test()
+		{
+			System.out.println("InnerClass test");
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		OuterClass obj=new OuterClass();
+		obj.demo();
+	}
+
+}
