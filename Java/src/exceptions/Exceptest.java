df3f4ac9e3f0d185389e6cfa0c77c38d18e3c9d1
Asad <asadmohammad8259@gmail.com>
2025-03-25 23:49:18 +0530

    Exceptest

---
+++ Java/src/exceptions/Exceptest.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+public class Exceptest
+{
+	int[] i= new int[5]; //creates an array of size 5
+	void test()
+	{
+		try
+		{
+		i[5] = 12/0;
+		}catch (Exception e){System.out.println(e);}
+	}
+	public static void main(String[] args)
+	{
+		Exceptest obj = new Exceptest();
+		System.out.println("Main method");
+		obj.test();
+		System.out.println("End of the Main");
+	}
+
+}
